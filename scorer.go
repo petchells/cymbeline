6b0a71105bde904515a41284b14648e0cba76e93
Paul Etchells (archon) <p.a.etchells@gmail.com>
2017-03-30 19:56:40 +0200

    added position weights

--- scorer.go
+++ scorer.go
@@ -1,5 +1,15 @@
 package main
 
+var posWeights = [][]int{
+	{4, -3, 2, 2, 2, 2, -3, 4},
+	{-3, -4, -1, -1, -1, -1, -4, -3},
+	{2, -1, 1, 0, 0, 1, -1, 2},
+	{2, -1, 0, 1, 1, 0, -1, 2},
+	{2, -1, 0, 1, 1, 0, -1, 2},
+	{2, -1, 1, 0, 0, 1, -1, 2},
+	{-3, -4, -1, -1, -1, -1, -4, -3},
+	{4, -3, 2, 2, 2, 2, -3, 4}}
+
 type ScoreParams struct {
 	// multiplier of piece count for each move
 	pieceCountWeights []int
