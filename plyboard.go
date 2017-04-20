d73c1857bb0eb657c566f12af7b33e7aae96269b
Etch (archon.pauletchells.com) <p.a.etchells@gmail.com>
2017-04-20 19:09:34 +0200

    wip: ply searches

--- plyboard.go
+++ plyboard.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"sort"
 )
 
@@ -22,49 +23,47 @@ type PlyBoard struct {
 	evaluationFunction func([8][8]Square, Square) float64
 }
 
-func (pb *PlyBoard) deepSearch(b *Board, myColour Square, depth int) *Move {
-	bcp := b.copy()
-	// TODO return result on a chan
-	moves := pb.findAllMoves(bcp, myColour)
-	if len(moves) == 0 {
+func (pb *PlyBoard) deepSearch(b *Board, myColour Square, depth int) Move {
+	best := Move{score: math.Inf(-1)}
+	recursiveSearch := func(b *Board, colour Square, depth int) Move {
+		pos := Position{}
+		bcp := &Board{}
+		oppColour := pb.oppColour(colour)
+		for i := 0; i < 8; i++ {
+			for j := 0; j < 8; j++ {
+				pos.x, pos.y = i, j
+				if b.isValidMove(pos, colour) {
+					bcp.copyFrom(b)
+					b.playMove(pos, colour)
+					if depth == 0 {
+						score := pb.evaluationFunction(bcp.rows, colour)
+						return Move{pos: &pos, score: score}
+					}
+					// switch sides and go deeper
+					m := pb.recursiveSearch(b, oppColour, depth-1)
+					if m.score > best.score {
+						best
+					}
+				}
+			}
+		}
 		return nil
 	}
-	if len(moves) == 1 {
-		return &moves[0]
-	}
-	sort.Sort(sort.Reverse(ByScore(moves)))
-	for i := 0; i < len(moves); i++ {
-		//pos := scores[i].pos
-		// opp has valid?
-		// swap piece
-		// find all moves
-		// go deeper
-		pb.xx(bcp, myColour, 2)
-		// else have i got valid?
-	}
-	bestMove := &moves[0]
-	return bestMove
+	return best
 }
 
 func (pb *PlyBoard) findAllMoves(b *Board, myColour Square) []Move {
 	var pos Position
 	moves := []Move{}
-	validMoves := b.findAllValidMoves(myColour)
-	for i := 0; i < len(validMoves); i++ {
-		bcp := b.copy()
-		bcp.playMove(&validMoves[i], myColour)
+	positions := b.findAllValidMoves(myColour)
+	bcp := &Board{}
+	for _, move := range positions {
+		bcp.copyFrom(b)
+		bcp.playMove(&move, myColour)
 		score := pb.evaluationFunction(bcp.rows, myColour)
 		moves = append(moves, Move{pos: &pos, score: score})
 	}
 	return moves
-}
-
-func (pb *PlyBoard) xx(b *Board, myPiece Square, depth int) {
-	if depth -= 1; depth == 0 {
-		// TODO do evaluation
-		return
-	}
-
 }
 
 func (pb *PlyBoard) oppColour(myColour Square) Square {
